2f0cd3b338f1f687a42986a0daca2a62c65422ef
PRBoyko <PRBoyko@gmail.com>
2021-06-10 19:53:16 +0300

    refactoring accordion icons

--- src/App.js
+++ src/App.js
@@ -2,29 +2,19 @@ import React from 'react';
 
 import { Title } from './lib';
 // import { TableProps, TabsProps } from './TestData';
-import DropDown from './lib/components/DropDown';
-import IncrementStepper from './lib/components/IncrementStepper';
-import './App.scss';
-import './lib/components/styles/_increment-stepper.scss';
+import Accordion from './lib/components/Accordion';
 
-const options = [
-  { value: 'English', label: 'English' },
-  { value: 'Ukrainian', label: 'Ukrainian' },
-  { value: 'Russian', label: 'Russian' }
-];
+import './App.scss';
+import './lib/components/styles/_accordion.scss';
 
 const App = () => (
   <div>
     <Title>Miramixi Storyteller</Title>
+    <Accordion
+      title ='This is title'
+      data = 'Here’s some example text that may answer an FAQ or give the user some helpful advice.'
+    />
 
-    <DropDown
-      options={options}
-      defaultValue='Language'
-      menuColor='blue' />
-
-   <IncrementStepper
-     increment='2'
-   />
   </div>
 
 );
