48a5ebee70582919b48e697d3d83383da2c39333
PRBoyko <PRBoyko@gmail.com>
2021-06-11 21:33:24 +0300

    tooltip ready

--- src/App.js
+++ src/App.js
@@ -4,12 +4,14 @@ import { Title } from './lib';
 // import { TableProps, TabsProps } from './TestData';
 import './App.scss';
 import './lib/components/styles/_tool-tip.scss';
+import ToolTip from './lib/components/ToolTip';
 
- const options = ['English', 'Ukrainian', 'English'];
+const options = ['English', 'Ukrainian', 'English'];
 
 const App = () => (
   <div>
     <Title>Miramixi Storyteller</Title>
+    <ToolTip data={options} arrow={true}/>
   </div>
 
 );
